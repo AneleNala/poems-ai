2a2da55fff7e7be109e9ed76067e8fb3c552ca00
Anele Naa <nalaanele00@gmail.com>
2024-02-03 13:13:08 +0200

    Added AI Integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-function generatePoem(event){
-  event.preventDefault();
+
+function displayPoem(response){
 
   new Typewriter("#poem", {
-  strings: "Uthando lubopha Ingqondo kanye nomoya wami ngabhande linye.",
+  strings: response.data.answer,
   autoStart: true,
   delay: 1,
   cursor: "",
 });
 }
+
+function generatePoem(event){
+event.preventDefault();    
+let userInput = document.querySelector("#user-input")    
+let apiKey = "9tc2ca054b9644af4a7a4ffo0b15b933";
+let context = "Generate a simple love and romance poem, in 4 line break format, follow the user instruction below and DO NOT INCLUDE THE user input theme IN THE SENTENCE "
+let prompt = `User instruction: Generate poem about a ${userInput.value} theme `;
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+axios.get(apiUrl).then(displayPoem);
+}
+
 
 let formElement = document.querySelector("#form");
 formElement.addEventListener("submit", generatePoem);
