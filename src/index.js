efa2a4ea6afd25240625be5bd1030a4fb1254024
Anele Naa <nalaanele00@gmail.com>
2024-02-05 11:47:58 +0200

    Improved user experience

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 
 function displayPoem(response){
-
   new Typewriter("#poem", {
   strings: response.data.answer,
   autoStart: true,
@@ -16,6 +15,11 @@ let apiKey = "9tc2ca054b9644af4a7a4ffo0b15b933";
 let context = "Generate a simple love and romance poem, in 4 line break format, follow the user instruction below and DO NOT INCLUDE THE user input theme IN THE SENTENCE "
 let prompt = `User instruction: Generate poem about a ${userInput.value} theme `;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+ 
+
+let poemElement = document.querySelector("#poem");
+poemElement.style.display = "block";
+poemElement.innerHTML = `<span class="blink">⏳Generating a poem about ${userInput.value}</span>`;
 
 axios.get(apiUrl).then(displayPoem);
 }
